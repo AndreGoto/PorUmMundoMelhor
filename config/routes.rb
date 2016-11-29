548a1be85c1f254250d2b1e2e96f6b4e5e5e9b5a
Fillype Farias <fillype1@hotmail.com>
2016-11-29 11:41:13 -0200

    add omniauth migration to user

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   resources :jobs, only: [:index, :show, :edit, :update, :new, :create]
   get 'dashboard/index'
 
-  devise_for :users
+  devise_for :users,
+  controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
 
-  devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
