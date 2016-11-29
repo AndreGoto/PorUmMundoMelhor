e51d63df78d5eaf378973913413586a842300aef
Fillype Farias <fillype1@hotmail.com>
2016-11-29 11:34:51 -0200

    add devise on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :jobs
   has_many :volunteers
+
+  devise :omniauthable, omniauth_providers: [:facebook]
 end
