9601f0941b1fc82f2ddad0146bf941e1f8738a68
Sonia Arai <soninhanaomi@gmail.com>
2016-11-29 11:39:23 -0200

    Implement seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+range = (1..5).to_a
+
+5.times do
+  user = User.new(
+      email: Faker::Internet.email,
+      password: "123456",
+      password_confirmation: "123456",
+      name: Faker::Name.name,
+      birthday: Faker::Date.backward(10000),
+      city: Faker::Address.city,
+      picture: Faker::Avatar.image,
+      about: Faker::Lorem.sentence
+  )
+  user.save!
+end
+
+10.times do
+  jobs =Job.new(
+    description: Faker::Lorem.paragraph,
+    short_description: Faker::Lorem.sentence,
+    category: Faker::Lorem.words,
+    title: Faker::Name.title,
+    remote: Faker::Boolean.boolean,
+    finish: Faker::Boolean.boolean,
+    address: Faker::Address.street_address,
+    date: Faker::Date.forward(30),
+    user_id: range.sample,
+    picture: Faker::Avatar.image
+  )
+  jobs.save!
+end
