5035956bb11c738df45b2fc1dd8177d3a2d1315e
andre goto <andre.yuji@live.com>
2016-11-30 11:57:17 -0200

    Implment apply to jobs

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  resources :jobs, only: [:index, :show, :edit, :update, :new, :create]
+  resources :jobs, only: [:index, :show, :edit, :update, :new, :create] do
+    resources :volunteers, only: [:new, :create]
+  end
   get 'dashboard/index'
 
   devise_for :users,
   controllers: { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'jobs#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
