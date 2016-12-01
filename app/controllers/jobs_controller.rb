557dc18ad59552d175de2fb2bb0041cea3a6efd3
Sonia Arai <soninhanaomi@gmail.com>
2016-12-01 15:51:17 -0200

    implement search view with map

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,21 +8,22 @@ class JobsController < ApplicationController
   def search
     @jobs = Job.all
     if params[:address].present?
-      @jobs = @jobs.where.not(latitude: nil, longitude: nil)
-      @hash = Gmaps4rails.build_markers(@jobs) do |job, marker|
-        marker.lat job.latitude
-        marker.lng job.longitude
-      end
-      @job_coordinates = ""
-      @jobs.each do |job|
-        @job_coordinates = { lat: job.latitude, lng: job.longitude }
-      end
+      @jobs = @jobs.near(params[:address], 20)
     end
     if params[:title].present?
       @jobs = @jobs.where('lower(title) LIKE ?', "%#{params[:title].downcase}%")
     end
     if params[:category].present?
       @jobs = @jobs.where(category: params[:category])
+    end
+    @jobs = @jobs.where.not(latitude: nil, longitude: nil)
+    @hash = Gmaps4rails.build_markers(@jobs) do |job, marker|
+      marker.lat job.latitude
+      marker.lng job.longitude
+    end
+    @job_coordinates = []
+    @jobs.each do |job|
+      @job_coordinates << { lat: job.latitude, lng: job.longitude }
     end
   end
 
