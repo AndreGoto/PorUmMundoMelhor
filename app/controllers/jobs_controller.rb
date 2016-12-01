67d8ef722d8fc2e1912e50b0597cd90f47f488d2
andre goto <andre.yuji@live.com>
2016-12-01 19:51:37 -0200

    Add volunteers picture for jobs#show

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -33,6 +33,12 @@ class JobsController < ApplicationController
       @job_applied = Volunteer.where(user_id: current_user.id, job_id: params[:id])
       @job_applied = Job.where(id: params[:id], user_id: current_user.id) if @job_applied.empty?
     end
+    volunteers = Volunteer.where(job_id: params[:id])
+    @volunteers = []
+    @volunteers << User.find(@job.user_id)
+    volunteers.each do |volunteer|
+      @volunteers << User.find(volunteer.user_id)
+    end
     @job_coordinates = { lat: @job.latitude, lng: @job.longitude }
   end
 
